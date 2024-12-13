f7c616d491e2d4a2d16afeea0847d6ccdb972be7
Shivd131 <shivdeshpande131@gmail.com>
2024-12-13 18:44:07 +0530

    feat: register form functionality

--- src/main/java/com/contact_hive/contact_hive/controllers/PageController.java
+++ src/main/java/com/contact_hive/contact_hive/controllers/PageController.java
@@ -3,6 +3,10 @@ package com.contact_hive.contact_hive.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.contact_hive.contact_hive.forms.UserForm;
 
 @Controller
 public class PageController {
@@ -28,7 +32,7 @@ public class PageController {
     }
 
     @GetMapping("/contact")
-    public String contactPage(Model model ) {
+    public String contactPage(Model model) {
         model.addAttribute("name", "This is contact");
         return "contact";
     }
@@ -41,7 +45,22 @@ public class PageController {
 
     @GetMapping("/register")
     public String registerPage(Model model) {
-        model.addAttribute("name", "This is register");
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
         return "register";
     }
+
+    @PostMapping("/register")
+    public String registerPagePost(@ModelAttribute UserForm userForm, Model model) {
+        System.out.println("Register page post method called");
+        //fetch form data
+        System.out.println(userForm);
+        
+        // Add attributes to the model if needed (e.g., success message)
+        model.addAttribute("success", "Registration successful!");
+
+        // Render the register page again
+        return "register";
+    }
+
 }
