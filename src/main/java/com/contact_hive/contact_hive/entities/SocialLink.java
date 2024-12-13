8f127342f895d44770d5e5e004dcb129074a5c6d
Shivd131 <shivdeshpande131@gmail.com>
2024-12-13 15:38:28 +0530

    fix

--- src/main/java/com/contact_hive/contact_hive/entities/SocialLink.java
+++ src/main/java/com/contact_hive/contact_hive/entities/SocialLink.java
@@ -1,6 +1,5 @@
 package com.contact_hive.contact_hive.entities;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
