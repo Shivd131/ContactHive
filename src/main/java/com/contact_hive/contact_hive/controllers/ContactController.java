d0873e33154f751e25202cc300e817cf75ccef48
Shivd131 <shivdeshpande131@gmail.com>
2024-12-29 15:55:16 +0530

    feat: start processing functionality

--- src/main/java/com/contact_hive/contact_hive/controllers/ContactController.java
+++ src/main/java/com/contact_hive/contact_hive/controllers/ContactController.java
@@ -1,13 +1,29 @@
 package com.contact_hive.contact_hive.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.contact_hive.contact_hive.forms.ContactForm;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
-    @RequestMapping("/add")
-    public String addContactView() {
+    @GetMapping("/add")
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
+        model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
     }
+
+    @PostMapping("/add")
+    public String saveContact(@ModelAttribute ContactForm contactForm) {
+        System.out.println(contactForm);
+
+        return "redirect:/user/contacts/add";
+    }
+
 }
