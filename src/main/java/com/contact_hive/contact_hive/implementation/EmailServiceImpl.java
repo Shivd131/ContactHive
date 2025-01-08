f7f4b3d2eaab902a1dd006109b35ca0e080c21b0
Shivd131 <shivdeshpande131@gmail.com>
2025-01-08 10:32:49 +0530

    feat: mailstrap-sending emails

---
+++ src/main/java/com/contact_hive/contact_hive/implementation/EmailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.contact_hive.contact_hive.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.contact_hive.contact_hive.services.EmailService;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+    @Value("${spring.mail.properties.domain_name}")
+    private String domainName;
+
+    @Override
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setFrom("domainName");
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendEmailWithAttachment(String to, String subject, String text, String pathToAttachment) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void sendEmailWithHtml(String to, String subject, String htmlBody) {
+        // TODO Auto-generated method stub
+    }
+
+}
--- src/test/java/com/contact_hive/contact_hive/ContactHiveApplicationTests.java
+++ src/test/java/com/contact_hive/contact_hive/ContactHiveApplicationTests.java
@@ -1,13 +1,25 @@
 package com.contact_hive.contact_hive;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.contact_hive.contact_hive.services.EmailService;
 
 @SpringBootTest
 class ContactHiveApplicationTests {
+	@Autowired
+	private EmailService emailService;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void SendMailTests() {
+
+		emailService.sendEmail("shivdeshpande131@gmail.com", "Email Email ahajhaha", "systum");
+
 	}
 
 }
