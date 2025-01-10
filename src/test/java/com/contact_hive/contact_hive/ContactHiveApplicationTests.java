578dd34d25c036244f5b645253a0412f35d5de66
Shivd131 <shivdeshpande131@gmail.com>
2025-01-10 23:21:12 +0530

    feat: containerize

--- src/test/java/com/contact_hive/contact_hive/ContactHiveApplicationTests.java
+++ src/test/java/com/contact_hive/contact_hive/ContactHiveApplicationTests.java
@@ -1,25 +1,25 @@
 package com.contact_hive.contact_hive;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.contact_hive.contact_hive.services.EmailService;
+// import com.contact_hive.contact_hive.services.EmailService;
 
 @SpringBootTest
 class ContactHiveApplicationTests {
-	@Autowired
-	private EmailService emailService;
+	// @Autowired
+	// private EmailService emailService;
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
-	@Test
-	void SendMailTests() {
+	// @Test
+	// void SendMailTests() {
 
-		emailService.sendEmail("shivdeshpande131@gmail.com", "Email Email ahajhaha", "systum");
+	// 	emailService.sendEmail("shivdeshpande131@gmail.com", "Email Email ahajhaha", "systum");
 
-	}
+	// }
 
 }
